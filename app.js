1c0bc6d1f9cf131358a9ff5aa8d4e92621a5caea
mahmuda_munny <67213233+munny006@users.noreply.github.com>
2023-03-01 00:29:26 +0600

    github button created

--- app.js
+++ app.js
@@ -1,36 +1,44 @@
-const handleSearch = () =>{
-    const inputValue = document.getElementById('input-value').value;
-    if(inputValue){
-        fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
-    .then((res)=>res.json())
-    .then((data)=>{
-       if(data.title == 'No Definitions Found' ){
-        alert(data.message);
-       }
-       else{
-        displayResult(data)
-       }
-    });
+const handleSearch = () => {
+    const inputValue = document.getElementById("input-value").value;
+    if (inputValue) {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.title === "No Definitions Found") {
+            alert(data.message);
+          } else {
+            displayResult(data);
+          }
+        });
+    } else {
+      alert("empty input field ");
     }
-    else{
-        alert('Empty Found');
-    }
-};
-const displayResult = (data) => {
-    const parent = document.getElementById('audio-container');
-   data[0].phonetics.forEach((element)=>{
-    const audio = document.createElement('audio');
-    audio.src = element.audio;
-    const button = document.createElement("button");
-    button.innerHTML = "Play";
-    button.onclick = () =>{
-        audio.play();
-    };
-    const container = document.createElement("div");
-    container.appendChild(button);
-    container.appendChild(audio);
-       
-    parent.appendChild(audio);
-   });
+  };
   
-}
+  const displayResult = (data) => {
+    const parent = document.getElementById("audio-container");
+    data[0].phonetics.forEach((element) => {
+      const audio = document.createElement("audio");
+      audio.src = element.audio;
+  
+      const button = document.createElement("button");
+      button.innerHTML = "Play";
+      button.onclick = () => {
+        audio.play();
+      };
+      const container = document.createElement("div");
+      container.appendChild(button);
+      container.appendChild(audio);
+  
+      parent.appendChild(container);
+    });
+  };
+
+
+  const loadGithubUsers = () =>{
+    fetch("https://api.github.com/users?per_page=10")
+    .then((res) => res.json())
+    .then((result) => {
+      console.log(result);
+    });
+  };
